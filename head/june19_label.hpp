1778b0f0aae67924a882de168552ca6154f4b2ae
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-05-11 18:29:25 +0200

    Labels

--- head/june19_label.hpp
+++ head/june19_label.hpp
@@ -22,13 +22,15 @@
 #include "june19_core.hpp"
 
 
-namespace june19{
-	const int LABEL_LEFT = 0;
-	const int LABEL_RIGHT = 1;
-	const int LABEL_CENTER = 2;
+namespace Slyvina {
+	namespace June19 {
+		const int LABEL_LEFT = 0;
+		const int LABEL_RIGHT = 1;
+		const int LABEL_CENTER = 2;
 
-	j19gadget* CreateLabel(std::string name, int x, int y, int w, int h, j19gadget* group, int style = LABEL_LEFT);
-	j19gadget* CreateLabelR(std::string name, int x, int y, int w, int h, j19gadget* group, int style = LABEL_LEFT);
+		j19gadget* CreateLabel(std::string name, int x, int y, int w, int h, j19gadget* group, int style = LABEL_LEFT);
+		j19gadget* CreateLabelR(std::string name, int x, int y, int w, int h, j19gadget* group, int style = LABEL_LEFT);
 
-	j19gadget* CreatePanel(int x, int y, int w, int h, j19gadget* group,j19ctype a=j19ctype::Absolute);
+		j19gadget* CreatePanel(int x, int y, int w, int h, j19gadget* group, j19ctype a = j19ctype::Absolute);
+	}
 }
--- src/june19_label.cpp
+++ src/june19_label.cpp
@@ -18,85 +18,88 @@
 // 3. This notice may not be removed or altered from any source distribution.
 // EndLic
 
-#include "june19_core.hpp"
-#include "june19_label.hpp"
+#include "../head/june19_core.hpp"
+#include "../head/june19_label.hpp"
 
-namespace june19{
+namespace Slyvina {
+	namespace June19 {
 
-	static std::string _error;
-	using namespace TrickyUnits;
+		static std::string _error;
+		using namespace Units;
+		using namespace TQSG;
 
-	static void DrawLabel(j19gadget*g){
-		j19chat("Drawing label: " << g->Caption << "(" << g->DrawX() << "," << g->DrawY() << ")");
-		_error = "";
-		auto F{ g->Font() };
-		if (!F) { _error = "No font for label"; j19chat("Font issue");  return; }
-		if (g->BA) {
-			TQSG_ACol(g->BR, g->BG, g->BB, g->BA);
-			TQSG_Rect(g->DrawX(), g->DrawY(), g->W(), g->H());
+		static void DrawLabel(j19gadget* g) {
+			j19chat("Drawing label: " << g->Caption << "(" << g->DrawX() << "," << g->DrawY() << ")");
+			_error = "";
+			auto F{ g->Font() };
+			if (!F) { _error = "No font for label"; j19chat("Font issue");  return; }
+			if (g->BA) {
+				SetColor(g->BR, g->BG, g->BB, g->BA);
+				Rect(g->DrawX(), g->DrawY(), g->W(), g->H());
+			}
+			SetColor(g->FR, g->FG, g->FB, g->FA);
+			switch (g->IntFlag) {
+			case 0:
+				F->Text(g->Caption, g->DrawX(), g->DrawY());
+				break;
+			case 1:
+				F->Text(g->Caption, g->DrawX() + g->W(), g->DrawY(), Align::Right);
+				break;
+			case 2:
+				F->Text(g->Caption, g->DrawX() + floor(g->W() / 2), g->DrawY(), Align::Center);
+				break;
+			default:
+				_error = "Unknown label alignment flag: " + std::to_string(g->IntFlag);
+			}
+
 		}
-		TQSG_ACol(g->FR, g->FG, g->FB, g->FA);
-		switch (g->IntFlag) {
-		case 0:
-			F->Draw(g->Caption, g->DrawX(), g->DrawY());
-			break;
-		case 1:
-			F->Draw(g->Caption, g->DrawX()+g->W(), g->DrawY(),1);
-			break;
-		case 2:
-			F->Draw(g->Caption, g->DrawX() + floor(g->W()/2), g->DrawY(), 2);
-			break;
-		default:
-			_error = "Unknown label alignment flag: " + std::to_string(g->IntFlag);
-		}
-		
-	}
 
 
-	j19gadget* CreateLabel(std::string name, int x, int y, int w, int h, j19gadget* group, int style) {
-		static auto init{ false };
-		auto ret{ new j19gadget() };
-		if (!init) {
-			j19gadget::RegDraw(j19kind::Label,DrawLabel);
+		j19gadget* CreateLabel(std::string name, int x, int y, int w, int h, j19gadget* group, int style) {
+			static auto init{ false };
+			auto ret{ new j19gadget() };
+			if (!init) {
+				j19gadget::RegDraw(j19kind::Label, DrawLabel);
+			}
+			_error = "";
+			ret->SetKind(j19kind::Panel);
+			ret->Caption = name;
+			ret->X(x);
+			ret->Y(y);
+			ret->W(w);
+			ret->H(h);
+			ret->SetParent(group);
+			ret->IntFlag = style;
+			return ret;
 		}
-		_error = "";
-		ret->SetKind(j19kind::Panel);
-		ret->Caption = name;
-		ret->X(x);
-		ret->Y(y);
-		ret->W(w);
-		ret->H(h);
-		ret->SetParent(group);
-		ret->IntFlag = style;
-		return ret;
-	}
-	j19gadget* CreateLabelR(std::string name, int x, int y, int w, int h, j19gadget* group, int style) {
-		auto ret{ CreateLabel(name,x,y,w,h,group,style) };
-		ret->X(x, j19ctype::Percent);
-		ret->Y(y, j19ctype::Percent);
-		ret->W(w, j19ctype::Percent);
-		ret->H(h, j19ctype::Percent);
-		return ret;
-	}
+		j19gadget* CreateLabelR(std::string name, int x, int y, int w, int h, j19gadget* group, int style) {
+			auto ret{ CreateLabel(name,x,y,w,h,group,style) };
+			ret->X(x, j19ctype::Percent);
+			ret->Y(y, j19ctype::Percent);
+			ret->W(w, j19ctype::Percent);
+			ret->H(h, j19ctype::Percent);
+			return ret;
+		}
 
-	static void DrawPanel(j19gadget* g){
-		TQSG_ACol(g->BR, g->BG, g->BB, g->BA);
-		TQSG_Rect(g->DrawX(), g->DrawY(), g->W(), g->H());
-	}
-	j19gadget* CreatePanel(int x, int y, int w, int h, j19gadget* group,j19ctype a) {
-		static auto init{ false };
-		auto ret{ new j19gadget() };
-		if (!init) {
-			j19gadget::RegDraw(j19kind::Panel, DrawLabel);
+		static void DrawPanel(j19gadget* g) {
+			SetColor(g->BR, g->BG, g->BB, g->BA);
+			Rect(g->DrawX(), g->DrawY(), g->W(), g->H());
 		}
-		_error = "";
-		ret->SetKind(j19kind::Panel);
-		ret->X(x,a);
-		ret->Y(y,a);
-		ret->W(w,a);
-		ret->H(h,a);
-		ret->BA = 255;
-		ret->SetParent(group);
-		return ret;
+		j19gadget* CreatePanel(int x, int y, int w, int h, j19gadget* group, j19ctype a) {
+			static auto init{ false };
+			auto ret{ new j19gadget() };
+			if (!init) {
+				j19gadget::RegDraw(j19kind::Panel, DrawLabel);
+			}
+			_error = "";
+			ret->SetKind(j19kind::Panel);
+			ret->X(x, a);
+			ret->Y(y, a);
+			ret->W(w, a);
+			ret->H(h, a);
+			ret->BA = 255;
+			ret->SetParent(group);
+			return ret;
+		}
 	}
 }
